d438a1dd576664c721026862d7797493a44cabba
mA4rK0 <onlymarko25@gmail.com>
2025-10-05 09:17:38 +0700

    feat: created table component in front-end

--- project-management-frontend/src/App.jsx
+++ project-management-frontend/src/App.jsx
@@ -7,6 +7,8 @@ import {
 } from '@mui/material';
 import { createBrowserRouter, Link, RouterProvider } from 'react-router';
 
+import Table from './components/ui/Table';
+
 const theme = createTheme({
   typography: {
     fontFamily: ['Roboto, sans-serif'].join(','),
@@ -18,8 +20,35 @@ const router = createBrowserRouter([
     path: '/',
     element: (
       <Box>
-        <Typography variant="h1">Home</Typography>
-        <Link to={'/login'}>Login</Link>
+        <Table
+          columns={[
+            {
+              id: 'task',
+              label: 'Task',
+            },
+            {
+              id: 'status',
+              label: 'Status',
+            },
+          ]}
+          data={[
+            {
+              id: 1,
+              task: 'Task 1',
+              status: 'New',
+            },
+            {
+              id: 2,
+              task: 'Task 2',
+              status: 'In progress',
+            },
+            {
+              id: 3,
+              task: 'Task 3',
+              status: 'Done',
+            },
+          ]}
+        />
       </Box>
     ),
   },
