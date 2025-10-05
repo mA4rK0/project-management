ea8b5715ec6a30c5091a73598cd11977ee2b825e
mA4rK0 <onlymarko25@gmail.com>
2025-10-05 21:38:51 +0700

    feat: created login page

---
+++ project-management-frontend/src/components/pages/Auth/Login/Login.jsx
@@ -0,0 +1,41 @@
+import { Paper, Stack } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+import Select from '../../../ui/Forms/Select';
+
+const Login = () => {
+  const { control } = useForm();
+
+  return (
+    <Stack
+      spacing={2}
+      alignItems={'center'}
+      justifyContent={'center'}
+      height={'100vh'}
+    >
+      <Paper sx={{ width: 600, padding: 2 }}>
+        <Select
+          name={'category'}
+          control={control}
+          label={'Choose Category'}
+          options={[
+            {
+              value: 'category 1',
+              label: 'category 1',
+            },
+            {
+              value: 'category 2',
+              label: 'category 2',
+            },
+            {
+              value: 'category 3',
+              label: 'category 3',
+            },
+          ]}
+        />
+      </Paper>
+    </Stack>
+  );
+};
+
+export default Login;
