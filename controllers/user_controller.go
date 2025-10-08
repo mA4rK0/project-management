5ba3e195d5f548982e22958690be7a4a1e996362
mA4rK0 <onlymarko25@gmail.com>
2025-10-08 23:32:01 +0700

    feat: created update user data components

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/jinzhu/copier"
 	"github.com/mA4rK0/project-management/models"
 	"github.com/mA4rK0/project-management/services"
@@ -106,4 +107,34 @@ func (c *UserController) GetUserPagination (ctx *fiber.Ctx) error {
 	}
 
 	return utils.SuccessPagination(ctx, "Data Found", userResp, meta)
+}
+
+func (c *UserController) UpdateUser (ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+	publicID, err := uuid.Parse(id)
+	if err != nil {
+		return utils.BadRequest(ctx, "Invalid ID Format", err.Error())
+	}
+
+	var user models.User
+	if err := ctx.BodyParser(&user); err != nil {
+		return utils.BadRequest(ctx, "Failed Parsing Data", err.Error())
+	}
+	user.PublicID = publicID
+
+	if err := c.service.Update(&user); err != nil {
+		return utils.BadRequest(ctx, "Failed Update Data", err.Error())
+	}
+
+	userUpdated, err := c.service.GetByPublicID(id)
+	if err != nil {
+		return utils.InternalServerError(ctx, "Failed Get Data", err.Error())
+	}
+
+	var userResp models.UserResponse
+	err = copier.Copy(&userResp, &userUpdated)
+	if err != nil {
+		return utils.InternalServerError(ctx, "Error Parsing Data", err.Error())
+	}
+	return utils.Success(ctx, "Success Update Data", userResp)
 }
--- services/user_service.go
+++ services/user_service.go
@@ -15,6 +15,7 @@ type UserService interface {
 	GetByID (id uint) (*models.User, error)
 	GetByPublicID (id string) (*models.User, error)
 	GetAllPagination (filter, sort string, limit, offset int) ([]models.User, int64, error)
+	Update (user *models.User) error
 }
 
 type userService struct {
@@ -63,4 +64,8 @@ func (s *userService) GetByPublicID (id string) (*models.User, error) {
 
 func (s *userService) GetAllPagination (filter, sort string, limit, offset int) ([]models.User, int64, error) {
 	return s.repo.FindAllPagination(filter, sort, limit, offset)
+}
+
+func (s *userService) Update (user *models.User) error {
+	return s.repo.Update(user)
 }
