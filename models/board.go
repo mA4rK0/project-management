0fdbbb28744840534f4632921f1780ea35945a74
mA4rK0 <onlymarko25@gmail.com>
2025-10-11 14:04:10 +0700

    feat: create boards table

--- models/board.go
+++ models/board.go
@@ -11,8 +11,8 @@ type Board struct {
 	PublicID   uuid.UUID `json:"public_id" db:"public_id"`
 	Title 		 string `json:"title" db:"title"`
 	Description string `json:"description" db:"description"`
-	OwnerID 		int64 `json:"owner_internal_id" db:"owner_internal_id" gorm:"owner_internal_id"`
-	OwnerPublicID uuid.UUID `json:"owner_public_id" db:"owner_public_id"`
+	OwnerID int64 `json:"owner_internal_id" db:"owner_internal_id" gorm:"column:owner_internal_id"`
+	OwnerPublicID uuid.UUID `json:"owner_public_id" db:"owner_public_id" gorm:"column:owner_public_id"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	Duedate *time.Time `json:"due_date,omitempty" db:"due_date"`
+	Duedate *time.Time `json:"due_date,omitempty" db:"due_date" gorm:"column:due_date"`
 }
