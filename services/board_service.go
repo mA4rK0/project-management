fd47d114927c4778c4580c3593c4b07b72499a5d
mA4rK0 <onlymarko25@gmail.com>
2025-10-22 14:13:21 +0700

    feat: created get all my board paginated components

--- services/board_service.go
+++ services/board_service.go
@@ -14,6 +14,7 @@ type BoardService interface{
 	GetByPublicID (publicID string) (*models.Board, error)
 	AddMembers (boardPublicID string, userPublicIDS []string) error
 	RemoveMembers (boardPublicID string, userPublicIDs []string) error
+	GetAllByUserPaginate (userID, filter, sort string, limit, offset int) ([]models.Board, int64, error)
 }
 
 type boardService struct {
@@ -119,4 +120,8 @@ func (s *boardService) RemoveMembers (boardPublicID string, userPublicIDs []stri
 	}
 
 	return s.boardRepo.RemoveMembers(uint(board.InternalID), membersToRemove)
+}
+
+func (s *boardService) GetAllByUserPaginate (userID, filter, sort string, limit, offset int) ([]models.Board, int64, error) {
+	return s.boardRepo.FindAllByUserPaginate(userID, filter, sort, limit, offset)
 }
