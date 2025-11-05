3e39d6b82672b1eb112a3b5d45a549321966cc2a
mA4rK0 <onlymarko25@gmail.com>
2025-11-05 17:22:39 +0700

    feat: created update list components

--- controllers/list_controller.go
+++ controllers/list_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/mA4rK0/project-management/models"
 	"github.com/mA4rK0/project-management/services"
 	"github.com/mA4rK0/project-management/utils"
@@ -25,4 +26,34 @@ func (c *ListController) CreateList(ctx *fiber.Ctx) error {
 	}
 
 	return utils.Success(ctx, "List Successfully Created", list)
+}
+
+func (c *ListController) UpdateList (ctx *fiber.Ctx) error {
+	publicID := ctx.Params("id")
+	list := new(models.List)
+
+	if err := ctx.BodyParser(list); err != nil {
+		return utils.BadRequest(ctx, "Failed Parsing Data", err.Error())
+	}
+	if _, err := uuid.Parse(publicID); err != nil {
+		return utils.BadRequest(ctx, "ID not valid", err.Error())
+	}
+
+	existingList, err := c.service.GetByPublicID(publicID)
+	if err != nil {
+		return utils.NotFound(ctx, "List Not Found", err.Error())
+	}
+	list.InternalID = existingList.InternalID
+	list.PublicID = existingList.PublicID
+
+	if err := c.service.Update(list); err != nil {
+		return utils.BadRequest(ctx, "Failed Update List", err.Error())
+	}
+
+	updatedList, err := c.service.GetByPublicID(publicID)
+	if err != nil {
+		return utils.NotFound(ctx, "List Not Found", err.Error())
+	}
+
+	return utils.Success(ctx, "Successfully Update List", updatedList)
 }
